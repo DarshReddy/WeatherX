6de817f364ddafb441897a5434cfb4a957507e5a
darshreddy <darshan.reddy@scaler.com>
2024-02-22 17:05:31 +0530

    Add styles,fonts,colors and layout components

--- app/src/main/java/com/apps10x/weatherx/WeatherActivity.kt
+++ app/src/main/java/com/apps10x/weatherx/WeatherActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.apps10x.weatherx.databinding.ActivityMainBinding
+import com.apps10x.weatherx.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
+    private var _binding: ActivityWeatherBinding? = null
     private val binding get() = _binding!!
 
     private val weatherViewModel: WeatherViewModel by viewModels()
@@ -18,7 +18,7 @@ class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
